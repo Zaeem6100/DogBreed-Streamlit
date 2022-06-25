7961023afd52d21e7780308d76247d240bd13760
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2022-06-25 23:34:31 +0500

    predict added

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ path = '/dog_breed_classifier.h5'
 filepath = '/file.txt'
 
 
+def predictions(model, breed_list, image):
+    probabilities = model.predict(helper.loadImage(image))
+    list = []
+    for i in probabilities[0].argsort()[-5:][::-1]:
+        print(probabilities[0][i], "  :  ", breed_list[i])
+        list.append(tuple(probabilities[0][i], breed_list[i]))
+    return list
 
 
 if __name__ == '__main__':
